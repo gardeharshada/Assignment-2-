4515013006bb9de4d8ad231d8b5527365a18bd15
gardeharshada <gardeharshada74@gmail.com>
2026-02-04 01:25:22 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,12 @@
+# WAP to calculate the area of triangle and rectangle:
+def area_Of_triangle(base, height):
+    return 0.5 * base * height
+
+def area_of_rectangle(length, width):
+    return length * width
+base = 10
+height = 5
+print("Area of triangle:", area_Of_triangle(base, height))
+length = 4
+width = 6
+print("Area of rectangle:", area_of_rectangle(length, width))
