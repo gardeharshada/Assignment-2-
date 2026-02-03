4772c990bbafe2b2c3cb83971a0011e87b94ff9a
gardeharshada <gardeharshada74@gmail.com>
2026-02-03 23:31:43 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,6 @@
+# convert temperature from celsius to fahrenheit:
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+celsius = 25 
+print(f"{celsius}) ºC  is {celsius_to_fahrenheit(celsius)} ºF
